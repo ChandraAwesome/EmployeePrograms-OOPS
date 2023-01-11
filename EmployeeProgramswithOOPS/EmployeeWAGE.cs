66adfe1f4d9c12ad27daacb08e05cc84f95b59f3
ChandarAwesome <ajaygames00Gmail.com>
2023-01-11 17:43:25 +0530

    [Chandra]-WAGE

---
+++ EmployeeProgramswithOOPS/EmployeeWAGE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeProgramswithOOPS
+{
+    public class EmployeeWAGE
+    {
+        public static void WAGE()
+        {
+            Console.WriteLine("WAGE");
+        }
+    }
+}
