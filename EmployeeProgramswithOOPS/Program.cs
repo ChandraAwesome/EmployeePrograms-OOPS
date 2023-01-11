88fa9d21c8ee0fd117a839c3ece8a4db7ec9c88e
ChandarAwesome <ajaygames00Gmail.com>
2023-01-11 16:42:30 +0530

    [Chandra]-Oops employee programs

---
+++ EmployeeProgramswithOOPS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeProgramswithOOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); //hello
+        }
+    }
+}
