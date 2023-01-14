83bdad283aff271e0096edf544157ca9309a3292
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-01-14 21:08:32 +0530

    [Chandra]-Calculate multiple company wages

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeeProgramswithOOPS
+{
+   public class Program
+    {
+         static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!"); //hello
+            //EmployeeAttendance.Attendance();
+            //EmployeeWAGE.WAGE()
+            //ComputeWAGEusingClassMethod.computeEmpWage();
+            MultipleCompanyWages.computeEmpWage("More", 25, 2, 15);
+            MultipleCompanyWages.computeEmpWage("Big Bazar", 3, 25, 16);
+
+
+        }
+    }
+}
